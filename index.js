56265975ff28bb80e203dd11428667202fec4afa
S_Janybekova <s.t.janibekova@gmail.com>
2019-12-31 16:00:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+document.querySelector("img").setAttribute("src", "images/dice" + Math.floor(Math.random()*6 +1) + ".png")
+document.querySelector("img.img2").setAttribute("src", "images/dice" + Math.floor(Math.random()* 6 +1) + ".png")
